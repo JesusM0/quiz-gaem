8c9058c64c228b0bad27a559b9e3b284e8ef94b5
Kos-Mos <ChuyM203@gmail.com>
2020-06-28 05:41:07 -0400

    added questions array with questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
 // Any essential variables
 // Create Start Function
 // Create Timer Function
+// Create array of Questions
 
 //Basic HTML Elements Here
 var headEl = document.querySelector("header");
@@ -51,6 +52,20 @@ var timer = function() {
     timerEl.textContent = remainingTime;
     remainingTime--;
 };
+
+var javaScriptQuestions = [
+    {question: "Inside which HTML element do we put the JavaScript?", option1: "<js>", option2: "<script>", option3: "<javascript>", option4: "<scripting>", answer: "4" },
+    {question: "Which of the following is not a valid JavaScript variable name?", option1: "2names", option2: "_first_and_last_names", option3: "FirstAndLast", option4: "None of the above", answer: "1" },
+    {question: "What is the correct JavaScript syntax to write 'Hello World?'", option1: "System.out.println('Hello World')", option2: "println ('Hello World')", option3: "document.write('Hello World')", option4: "response.write('Hello World')", answer: "3" },
+    {question: "JavaScript entities start with _______ and end with _________.", option1: "Semicolon, colon", option2: "Semicolon, Ampersand", option3: "Ampersand, colon", option4: "Ampersand, Semicolon", answer: "4" },
+    {question: "Which of the following is the structure of an if statement?", option1: "if (conditional expression is true) thenexecute this codeend if", option2: "if (conditional expression is true)execute this codeend if", option3: "if (conditional expression is true)   {then execute this code>->}", option4: "if (conditional expression is true) then {execute this code}", answer: "3" },
+    {question: "What are variables used for in JavaScript Programs?", option1: "Storing numbers, dates, or other values", option2: "Varying randomly", option3: "Causing high-school algebra flashbacks", option4: "None of the above", answer: "1" },
+    {question: "Which of the following is not considered a JavaScript operator?", option1: "new", option2: "this", option3: "delete", option4: "typeof", answer: "2" },
+    {question: "JavaScript is interpreted by _________", option1: "Client", option2: "Server", option3: "Object", option4: "None Of The Above", answer: "1" },
+    {question: "Using ____ statement is how you test for a specific condition.", option1: "Select", option2: "If", option3: "Switch", option4: "For", answer: "2" },
+    {question: "Which of the following is the structure of an if statement?", option1: "if (conditional expression is true) thenexecute this codeend if", option2: "if (conditional expression is true)execute this codeend if", option3: "if (conditional expression is true)   {then execute this code>->}", option4: "if (conditional expression is true) then {execute this code}", answer: "3" },
+    
+];
 
 //Run quizStart();
 startBtn.onclick = quizStart;
