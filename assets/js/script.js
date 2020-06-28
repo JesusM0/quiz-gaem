aaa354e001bfb10680ab8e323daf5a39ba06cf4e
Kos-Mos <ChuyM203@gmail.com>
2020-06-28 04:52:43 -0400

    added start quiz function along with event

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
 var headEl = document.querySelector("header");
 var bodyEl = document.querySelector("body");
 var titleEl = document.querySelector("#page-title");
+var descriptionEl = document.querySelector("#description");
 
 //Specific Id/Classes Here
 var viewEL = document.querySelector("#view-scores");
@@ -20,3 +21,22 @@ var choiceFour = document.querySelector("#btn-4");
 //Essential Variables Here
 var remainingTime = 100;
 var score = 0;
+//Add New countdown var
+var countdown = "";
+
+var quizStart = function() {
+    //First, hide opening elements by applying "hide" class containing display: none; property
+    headEl.classList.add("hide");
+    titleEl.classList.add("hide");
+    viewEL.classList.add("hide");
+    startBtn.classList.add("hide");
+    descriptionEl.classList.add("hide");
+    //Display Questions and Answers by removing "hide" class
+    choicesEl.classList.remove("hide");
+    //Timer
+    countdown = setInterval(timer, 1000);
+
+};
+
+//Run quizStart();
+startBtn.onclick = quizStart;
