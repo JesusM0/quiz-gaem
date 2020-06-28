ac011222a735af88ccf60bd21dff96cbd31a6c73
Kos-Mos <ChuyM203@gmail.com>
2020-06-28 03:58:40 -0400

    added content/css for main, started script

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+//Basic HTML Elements Here
+//Note To Self: Make Sure to Console.log to check if selected correctly
+var headEl = document.querySelector("header");
+var bodyEl = document.querySelector("body");
+var titleEl = document.querySelector("#page-title");
+
+//Specific Id/Classes Here
+var viewEL = document.querySelector("#view-scores");
+var timerEl = document.querySelector("#timer");
+var questionEl = document.querySelector("#questions");
+var choicesEl = document.querySelector("#choices");
+
+//Button Selectors Here
+var startBtn = document.querySelector("#start");
+var choiceOne = document.querySelector("#btn-1");
+var choiceTwo = document.querySelector("#btn-2");
+var choiceThree = document.querySelector("#btn-3");
+var choiceFour = document.querySelector("#btn-4");
+
+//Essential Variables Here
+var remainingTime = 100;
+var score = 0;
